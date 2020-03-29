07668cab210a9c2f710cf1ffce88c49754e73acd
Chris Sanchez <chrisvsanchez@gmail.com>
2020-03-29 21:39:55 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,6 @@
 
 function callMe() {
-  let lyric = "mabye";
+  var lyric = "mabye";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
