5cda1e2c7bf5139f2db5b5541bd039a165583b1c
Chris Sanchez <chrisvsanchez@gmail.com>
2020-03-29 21:37:33 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,6 @@
 
 function callMe() {
- var lyric = "mabye";
+  var lyric = "mabye";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
@@ -9,9 +9,9 @@ function callMe() {
 }
 
 
-var crazy = function crazy() {
+  function crazy() {
   // fix the code in here:
-  function thisIsCrazy()
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!");
@@ -23,11 +23,11 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+  function sayMy(){
 
-  function sayMy() {
     var name = "Kristin";
     console.log(name);
  
   }
+  sayMy();
 }
